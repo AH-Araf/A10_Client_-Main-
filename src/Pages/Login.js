f96e5e546b4c0b3bd2ed3c07ff9fede1ece4bbbc
Araf <arafathossain.baiust@gmail.com>
2022-10-25 17:57:36 +0600

    Icons added

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,25 +1,33 @@
 import React from 'react';
+import { ButtonGroup, ListGroup } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 const Login = () => {
     return (
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+        <div>
+        <Form className='ms-lg-5 me-lg-5 ms-3 me-3'>
+            <Form.Group className="mb-4" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control name="email" type="email" placeholder="Enter email" required />
 
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group className="mb-4" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
                 <Form.Control name="password" type="password" placeholder="Password" required />
             </Form.Group>
 
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" className='mb-5'>
                 Login
-            </Button>
+            </Button> <br />
+
+            <ButtonGroup vertical className='mb-5 d-flex justify-content-center'>
+                <Button  className='mb-2' variant="outline-primary">  Login with Google</Button>
+                <Button variant="outline-dark">  Login with Github</Button>
+            </ButtonGroup>
         </Form>
+        </div>
     );
 };
 
